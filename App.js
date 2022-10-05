5203151f9f57f99e42346ac30c50ffd93e7cf4d6
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-10-05 22:06:10 +0200

    setup

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Mobile Development</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
